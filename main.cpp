fa51c4465540ece631b382de418a78b33e5ef4bc
ando <ando@emlab.cei.uec.ac.jp>
2025-06-13 11:27:27 +0900

    Add function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int function(int x){
+  return x*x;
+}
+
 int main(void){
 
   std::cout << "git" << std::endl;
@@ -8,6 +12,8 @@ int main(void){
   for(int i = 0; i < 10; i++){
     std::cout << i << std::endl;
   }
+
+  std::cout << function(3) << std::endl;
 
   return 0;
 }
